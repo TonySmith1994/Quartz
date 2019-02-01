7aec58158d4dddc583be2cb5b9ab2766bb6fa125
TonySmith1994 <1351262709@qq.com@qq.com>
2019-02-01 10:45:37 +0800

    使用反射获取类进行调度

--- src/main/java/QuartzSimple.java
+++ src/main/java/QuartzSimple.java
@@ -1,3 +1,5 @@
+import org.quartz.Job;
+
 /**
  * [调度类]
  *
@@ -7,14 +9,15 @@
  */
 
 public class QuartzSimple {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ClassNotFoundException, IllegalAccessException, InstantiationException {
         //简单任务调度，每隔多少时间执行一次，执行n次
         //SchedulerUtil.handleSimpleTrigger("简单调度", "测试组", "触发器名字", "触发器组名字", MyJob.class, 1, 8);
         //复杂调度，每天的什么时候执行任务
         //SchedulerUtil.hadleCronTrigger("复杂调度", "测试组", "触发器名字", "触发器组名字", MyJob.class, "0 */1 * * * ?");
         QuartzManager quartzManager = new QuartzManager();
+        Job job = (Job) Class.forName("MyJob").newInstance();
         //添加一个任务
-        quartzManager.addJob("任务一", "组一", "触发器名字", "触发器组名", MyJob.class, "0/1 * * * * ?");
+        quartzManager.addJob("任务一", "组一", "触发器名字", "触发器组名", job.getClass(), "0/1 * * * * ?");
         //开始所有任务
         quartzManager.startJobs();
         //睡眠
