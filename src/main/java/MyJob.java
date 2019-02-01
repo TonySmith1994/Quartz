f782424b6c98f1e2c005570a0a0aacf932724a33
TonySmith1994 <1351262709@qq.com@qq.com>
2019-02-01 08:58:34 +0800

    去掉任务类的异常

--- src/main/java/MyJob.java
+++ src/main/java/MyJob.java
@@ -1,20 +1,19 @@
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+
 
 import java.util.Date;
 
 /**
  * [任务类]
+ *
  * @author Tony
  * @date 2019-01-31
  * @copyright copyright (c) 2019
  */
 public class MyJob implements Job {
-
     @Override
-    public void execute(JobExecutionContext arg0) throws JobExecutionException {
-        System.out.println("Hello World"+new Date());
-
+    public void execute(JobExecutionContext arg0) {
+        System.out.println("Hello World[" + new Date()+"]");
     }
 }
